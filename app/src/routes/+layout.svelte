52be2a5d78eb69fc9d0233da8f1df9dbd71a3269
ZoneTwelve <zonetwelve159@gmail.com>
2023-09-04 23:49:56 +0800

    [update] basic structure

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<h1 class="text-3xl font-bold underline">
+		Hello world!
+	</h1>
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>查看 <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style lang="postcss">
+  :global(h1) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
