fed33e5cbbb62c630c4cb704d0e3179d7fb041d8
jackshinshin <curryisalegend@gmail.com>
2024-04-11 20:25:09 +0800

    Extracts selection list as a layout component

--- app/src/components/SymptomMenu.svelte
+++ app/src/components/SymptomMenu.svelte
@@ -1,4 +1,5 @@
 <script context="module" lang="ts">
+	import SelectionList from './layouts/SelectionList.svelte';
 	import { writable } from 'svelte/store';
 	export type SymptomData = {
 		part: string;
@@ -12,10 +13,11 @@
 	import { focus_body } from '$components/HumanBeing.svelte';
 	import { twMerge } from '$lib/helper';
 	import { ChevronLeft } from 'svelte-heros-v2';
-	import { setContext } from 'svelte';
+	import { onMount, setContext } from 'svelte';
 	import Overlay from './Overlay.svelte';
 	import SymptomSlider from './gadgets/SymptomSlider.svelte';
-	import { bodyPartChinese, symptomsList } from '$lib/constants';
+	import { bodyPartChinese, symptomDictionary, symptomsList } from '$lib/constants';
+	import { page } from '$app/stores';
 
 	setContext('selected_symptoms', selected_symptoms);
 
@@ -33,61 +35,57 @@
 	export function clearSelected() {
 		selected_symptoms.set([]);
 	}
-	export let selected_body_part;
+	export let selected_body_part = '';
 	export let menu_hide = true;
 	export let symptoms_selected_pass = false;
-	let selected_symptoms_count;
+	let selected_symptoms_count = 0;
 
 	$: menu_hide = selected_body_part === undefined;
 	$: selected_symptoms_count = $selected_symptoms.length;
 	$: symptoms_selected_pass = $selected_symptoms.length > 0;
+
+	onMount(() => {
+		const body_part = $page.url.searchParams.get('body_part') || '';
+		const recentSymptom = $page.url.searchParams.get('name') || '';
+		if (recentSymptom) {
+			const symptomChinese = symptomDictionary[recentSymptom];
+			selected_symptoms.set([...$selected_symptoms, { part: body_part, symptom: symptomChinese }]);
+			console.log(document.getElementById(symptomChinese));
+			document.getElementById(symptomChinese)?.click();
+		}
+	});
 </script>
 
 <Overlay showOverlay={!menu_hide} handleOverlayClick={cancelSelection} />
-<div
-	class={twMerge('fixed bg-white-f w-full bottom-0 border-t', {
-		'display-none': menu_hide
-	})}
+<SelectionList
+	{cancelSelection}
+	showList={menu_hide}
+	title={selected_body_part ? bodyPartChinese[selected_body_part] : ''}
 >
-	<div
-		class={twMerge('bg-white-f rounded-t-lg border-t-2 border-main-lighter', {
-			'mt-0 animate-slideDown fill-mode-forwards': menu_hide,
-			'mt-[-300px] opacity-100 relative animate-slideUp': !menu_hide
-		})}
-	>
-		<div class="py-[10px] bg-main-lightest px-5 flex items-center">
-			<ChevronLeft on:click={cancelSelection} class="mx-2 outline-none" />
-			<span>{selected_body_part ? bodyPartChinese[selected_body_part] : ''}</span>
+	{#if selected_body_part}
+		<div class="overflow-scroll h-[260px] z-0 flex flex-col">
+			{#each symptomsList[selected_body_part] as symptom}
+				<SymptomBlock
+					{symptom}
+					{selected_body_part}
+					extraStyle="shrink-0"
+					showWarning={symptom == '胸痛' ? true : false}
+				/>
+			{/each}
 		</div>
-		{#if selected_body_part !== undefined}
-			<div class="overflow-scroll h-[260px] z-0 flex flex-col">
-				{#each symptomsList[selected_body_part] as symptom}
-					<SymptomBlock {symptom} {selected_body_part} extraStyle="shrink-0" />
-				{/each}
+	{/if}
+	<div class="next-process p-5 border-t">
+		<div class="grid grid-cols-2 mb-4">
+			<div class="symptoms-selection text-blue-4">
+				已選症狀：{selected_symptoms_count}
 			</div>
+			<button class="text-right text-xs" on:click={clearSelected}>清空症狀</button>
+		</div>
+		{#if symptoms_selected_pass}
+			<SymptomSlider extraStyle="mb-4" />
 		{/if}
-		<div class="next-process p-5 border-t">
-			<div class="grid grid-cols-2 mb-4">
-				<div class="symptoms-selection text-blue-4">
-					已選症狀：{selected_symptoms_count}
-				</div>
-				<button class="text-right text-xs" on:click={clearSelected}>清空症狀</button>
-			</div>
-			{#if symptoms_selected_pass}
-				<SymptomSlider extraStyle="mb-4" />
-			{/if}
-			{#if symptoms_selected_pass}
-				<a href="/search/result">
-					<div
-						class={twMerge('bg-neutral-300 text-white text-center p-2 rounded-lg', {
-							'bg-gradient-to-r from-main-lighter to-main-light': symptoms_selected_pass
-						})}
-					>
-						下一步
-					</div>
-				</a>
-			{/if}
-			{#if !symptoms_selected_pass}
+		{#if symptoms_selected_pass}
+			<a href="/search/result">
 				<div
 					class={twMerge('bg-neutral-300 text-white text-center p-2 rounded-lg', {
 						'bg-gradient-to-r from-main-lighter to-main-light': symptoms_selected_pass
@@ -95,7 +93,16 @@
 				>
 					下一步
 				</div>
-			{/if}
-		</div>
+			</a>
+		{/if}
+		{#if !symptoms_selected_pass}
+			<div
+				class={twMerge('bg-neutral-300 text-white text-center p-2 rounded-lg', {
+					'bg-gradient-to-r from-main-lighter to-main-light': symptoms_selected_pass
+				})}
+			>
+				下一步
+			</div>
+		{/if}
 	</div>
-</div>
+</SelectionList>
---
+++ app/src/components/layouts/SelectionList.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { twMerge } from '$lib/helper';
+	import { ChevronLeft } from 'svelte-heros-v2';
+
+	export let cancelSelection = () => {};
+	export let title = '';
+	export let showList = false;
+</script>
+
+<div
+	class={twMerge('fixed bg-white-f w-full bottom-0 border-t', {
+		'display-none': showList
+	})}
+>
+	<div
+		class={twMerge('bg-white-f rounded-t-lg border-t-2 border-main-lighter', {
+			'mt-0 animate-slideDown fill-mode-forwards': showList,
+			'mt-[-300px] opacity-100 relative animate-slideUp': !showList
+		})}
+	>
+		<div class="py-[10px] bg-main-lightest px-5 flex items-center">
+			<ChevronLeft on:click={cancelSelection} class="mx-2 outline-none" />
+			<span>{title}</span>
+		</div>
+		<slot />
+	</div>
+</div>
