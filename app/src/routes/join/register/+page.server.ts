f212add3c947471c1ab3d10b8a3580a0b28ed46b
jackshinshin <curryisalegend@gmail.com>
2024-05-02 22:48:21 +0800

    [update]Register server action

--- app/src/routes/join/register/+page.server.ts
+++ app/src/routes/join/register/+page.server.ts
@@ -1,20 +1,24 @@
-import type { Actions, RequestEvent, ActionFailure, Redirect } from '@sveltejs/kit';
+import type { Actions, RequestEvent } from '@sveltejs/kit';
+import axios from 'axios';
 import { redirect } from '@sveltejs/kit';
 
-export async function load({ cookies }) {
-	const authToken = cookies.get('authToken');
-	// if (!authToken) {
-	// 	throw redirect(302, '/login');
-	// }
-}
-
 export const actions: Actions = {
-	default: async ({ cookies, request }: RequestEvent) => {
+	default: async ({ request }: RequestEvent) => {
 		const formData = await request.formData();
-		console.log('Data Received');
 
-		console.log(...formData);
+		const accountPayload = {
+			username: formData.get('email'),
+			password: formData.get('password')
+		};
+		console.log(accountPayload);
 
-		// cookies.set('authToken', authToken);
+		try {
+			await axios.post('http://127.0.0.1:8000/api/user', accountPayload);
+		} catch (error) {
+			return { failure: true };
+		}
+		console.log('Success!');
+
+		throw redirect(303, '/join/login');
 	}
 };
